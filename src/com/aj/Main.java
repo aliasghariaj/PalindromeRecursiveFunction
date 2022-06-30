e92cbb56a24f6c7aed90abcd5f5474dcf3835e8f
Ali <50335998+aliasghariaj@users.noreply.github.com>
2022-06-30 19:10:45 +0430

    Initial commit

--- src/com/aj/Main.java
+++ src/com/aj/Main.java
@@ -10,15 +10,15 @@ public class Main {
         System.out.print("Enter a word: ");
         String word = input.nextLine();
 
-        System.out.println("Result: " + isPalin(word));
+        System.out.println("Result: " + isPalindrome(word));
     }
 
     // method that checks palindrome of a string
-    private static boolean isPalin(String word) {
+    private static boolean isPalindrome(String word) {
         if (word.length() <= 1) return true;
 
         int stringLength = word.length();
 
-        return (word.charAt(0) == word.charAt(stringLength - 1) && isPalin(word.substring(1, stringLength - 1)));
+        return (word.charAt(0) == word.charAt(stringLength - 1) && isPalindrome(word.substring(1, stringLength - 1)));
     }
 }
